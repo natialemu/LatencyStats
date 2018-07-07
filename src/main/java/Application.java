8de53d3fda2d6bba740bca313dbab58762e1bc3c
Nathnael Alemu <nalemu@luc.edu>
2018-07-07 01:28:01 -0700

    converted to Spring boot app

---
+++ src/main/java/Application.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+//@Import(EmbeddedTomcatConfiguration.class)
+public class Application {
+
+    protected Application() {
+        // do nothing
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
