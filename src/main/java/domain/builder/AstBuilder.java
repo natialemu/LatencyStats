a7140a0b92afb29ae9b4699ec616085e078b0ea7
Nathnael Alemu <nalemu@luc.edu>
2018-07-18 09:58:24 -0700

    completed implementation of AstBuilder

--- src/main/java/domain/builder/AstBuilder.java
+++ src/main/java/domain/builder/AstBuilder.java
@@ -4,10 +4,7 @@ import dal.LatencyDAO;
 import dal.LatencyDaoImpl;
 import domain.MethodBean;
 import domain.abstraction.*;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.List;
-import java.util.Scanner;
 
 public class AstBuilder {
     private LatencyDAO latencyDAO;
@@ -31,92 +28,20 @@ public class AstBuilder {
             insertIntoApplication(components);
         }
 
-        ServiceAST serviceAST=null;
+        return root;
 
-        /**
-         * Use DAOs to get the neccessary data to build AST
-         */
-        try {
-            File file =
-                    new File("");
-            Scanner sc = new Scanner(file);
-            while (sc.hasNextLine()){
-
-                String[] currentLine = sc.nextLine().split(",");
-                assert(currentLine.length == 3);
-
-                String[] methodSignature = currentLine[0].split(".");
-
-
-                if(serviceAST == null){
-                    serviceAST = new ApplicationAbs();
-                    serviceAST.setName(applicationName);
-                }
-                PackageAbs packageAbs = parsePackage(methodSignature,serviceAST);
-                ClassAbs classAbs = parseClass(packageAbs.getName(),packageAbs,methodSignature[methodSignature.length-2],serviceAST);
-                MethodAbs methodAbs = parseMethod(classAbs.getName(),classAbs,methodSignature[methodSignature.length-1],serviceAST,Integer.parseInt(currentLine[2]));
-            }
-        }catch (FileNotFoundException fnfe){
-            fnfe.printStackTrace();
-        }
-        return serviceAST;
     }
 
     private void insertIntoApplication(List<ServiceAST> components) {
-        //TODO
-    }
 
-    private static ClassAbs parseClass(String currentPackageName, ServiceAST parent, String simpleclassName, ServiceAST serviceAST){
+        ServiceAST currentRoot = root;
+        for(ServiceAST component: components){
 
-        String className = currentPackageName + "." + simpleclassName;
-        ClassAbs classAbs = new ClassAbs();
-        classAbs.setName(className);
-        if(!serviceAST.contains(classAbs)){
-            serviceAST.addAsChildOf(parent,classAbs);
-        }
-        return classAbs;
-
-    }
-
-    private static MethodAbs parseMethod(String className, ServiceAST parent, String simpleMethodName, ServiceAST serviceAST,int executionTime){
-
-        String methodName = className + "." +simpleMethodName;
-        MethodAbs methodAbs = new MethodAbs();
-        methodAbs.setName(methodName);
-        Integer overallExecutionTime = executionTime;
-        methodAbs.setExecutionTime(overallExecutionTime);
-
-        if(!serviceAST.contains(methodAbs)){
-            serviceAST.addAsChildOf(parent,methodAbs);
-        }
-        return methodAbs;
-
-    }
-
-    private static PackageAbs parsePackage(String[] methodSignature, ServiceAST serviceAST){
-
-        PackageAbs currentPackage = new PackageAbs();
-        String currentPackageName = methodSignature[0];
-        currentPackage.setName(currentPackageName);
-
-        if(!serviceAST.contains(currentPackage)){
-            serviceAST.add(currentPackage);
-        }
-
-        for(int i = 1; i < methodSignature.length - 3; i++){
-            PackageAbs newPackage = new PackageAbs();
-            String packageName = currentPackageName + "." + methodSignature[i];
-            newPackage.setName(packageName);
-
-            if(!serviceAST.contains(newPackage)){
-                serviceAST.addAsChildOf(currentPackage,newPackage);
+            if(!currentRoot.getChildren().contains(component)){
+                currentRoot.add(component);
             }
-
-            currentPackageName = packageName;
-            currentPackage = newPackage;
-
+            currentRoot = component;
         }
-        return currentPackage;
 
     }
 
